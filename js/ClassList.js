1e7c1e86813399be0ef6add5518c2a6994d0f0fe
Step7750 <Step7750@gmail.com>
2016-08-15 19:45:04 -0600

    Added names and desc to accordion

--- js/ClassList.js
+++ js/ClassList.js
@@ -24,7 +24,7 @@ class ClassList {
             $.getJSON(self.baseURL + "unis/" + self.uni + "/" + self.term + "/all", function(data) {
                 self.classdata = data["classes"];
                 self.rmpdata = data["rmp"];
-                
+
                 loading.remove(function () {
                     // Remove the loading animation and populate the list
                     self.populateClassList(data["classes"], $("#courseSelector").find("#classdata"), "");
@@ -33,6 +33,55 @@ class ClassList {
         });
     }
 
+    generateClassDesc(desc) {
+        var html = '<div class="accordiondesc">';
+
+        if (desc["aka"] != undefined) {
+            html += "AKA: " + desc["aka"] + "<br>";
+        }
+        if (desc["desc"] != undefined) {
+            html += desc["desc"] + "<br><br>";
+        }
+
+        if (desc["units"] != undefined) {
+            html += desc["units"] + " units; ";
+
+            if (desc["hours"] == undefined) {
+                html += "<br>";   
+            }
+        }
+
+        if (desc["hours"] != undefined) {
+
+            html += desc["hours"] + "<br>";
+        }
+
+        if (desc["prereq"] != undefined) {
+            html += "<br>Prereq: " + desc["prereq"] + "<br>";
+        }
+
+        if (desc["coreq"] != undefined) {
+            html += "<br>Coreq: " + desc["coreq"] + "<br>";
+        }
+
+        if (desc["antireq"] != undefined) {
+            html += "<br>Antireq: " + desc["antireq"] + "<br>";
+        }
+
+        if (desc["notes"] != undefined && desc["notes"] != "") {
+            html += "<br>Notes: " + desc["notes"];
+        } 
+
+
+        return html;
+    }
+
+    populateClass(data, element) {
+        if (data["description"] != undefined) {
+            element.append(this.generateClassDesc(data["description"]));
+        }
+        console.log(data);
+    }
     /*
         Populates the classlist on demand given the hierarchy
     */
@@ -42,46 +91,60 @@ class ClassList {
         // Slide up the element
         element.slideUp(function () {
             for (var val in data) {
+                if (val == "classes") {
+                    // This is a class, process it differently
+                    self.populateClass(data, element)
+                }
+                else if (val != "description") {
+                    // Generate this new element, give it the path
+                    var thispath = path + "\\" + val;
 
-                // Generate this new element, give it the path
-                var thispath = path + "\\" + val;
-                var thiselement = $(self.generateAccordionHTML(val, thispath));
+                    var name = val;
 
-                thiselement.find("label").click(function () {
-                    // Onclick handler
-
-                    // do we need to close the element?
-                    if ($(this).attr("accordopen") == "true") {
-                        // Close the element
-                        $(this).attr("accordopen", "false");
-                        $(this).parent().find("ul").slideUp(function () {
-                            $(this).empty();
-                        })
-
+                    if (data[val]["description"] != undefined) {
+                        if (data[val]["description"]["name"] != undefined) {
+                            name += " - " + data[val]["description"]["name"]
+                        }
                     }
-                    else {
-                        // Open accordion
-                        var thispath = $(this).attr("path").split("\\");
-                        $(this).attr("accordopen", "true");
 
-                        // Element to populate
-                        var element = $(this).parent().find("ul");
+                    var thiselement = $(self.generateAccordionHTML(name, thispath));
 
-                        // want to find the data to populate
-                        var thisdata = self.classdata;
-                        for (var key in thispath) {
-                            if (key > 0) {
-                                thisdata = thisdata[thispath[key]];
+                    thiselement.find("label").click(function () {
+                        // Onclick handler
+
+                        // do we need to close the element?
+                        if ($(this).attr("accordopen") == "true") {
+                            // Close the element
+                            $(this).attr("accordopen", "false");
+                            $(this).parent().find("ul").slideUp(function () {
+                                $(this).empty();
+                            })
+
+                        }
+                        else {
+                            // Open accordion
+                            var thispath = $(this).attr("path").split("\\");
+                            $(this).attr("accordopen", "true");
+
+                            // Element to populate
+                            var element = $(this).parent().find("ul");
+
+                            // want to find the data to populate
+                            var thisdata = self.classdata;
+                            for (var key in thispath) {
+                                if (key > 0) {
+                                    thisdata = thisdata[thispath[key]];
+                                }
                             }
+                            
+                            // Populate the element
+                            self.populateClassList(thisdata, element, $(this).attr("path"));
                         }
                         
-                        // Populate the element
-                        self.populateClassList(thisdata, element, $(this).attr("path"));
-                    }
-                    
 
-                });
-                element.append(thiselement);
+                    });
+                    element.append(thiselement);
+                }
             }
 
             element.slideDown();
