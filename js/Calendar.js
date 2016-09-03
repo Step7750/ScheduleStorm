f6426ab126a2612f72baced2cb200aba0ea20686
Step7750 <Step7750@gmail.com>
2016-09-03 17:31:42 -0600

    Converted calendar to 15min segments

---
+++ js/Calendar.js
@@ -0,0 +1,8 @@
+class Calendar {
+    // Handles the UI construction of the calendar
+    constructor() {
+        console.log("Setting up calendar");
+
+        //<td><div class="event double">12:00–1:00 Meeting</div></td>
+    }
+}
