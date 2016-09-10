e7b6681dbfc18eb3a8849338de12b3e8b09ef554
Step7750 <Step7750@gmail.com>
2016-09-10 12:41:48 -0600

    Added preference saving in localStorage

--- js/Preferences.js
+++ js/Preferences.js
@@ -1,13 +1,29 @@
 class Preferences {
     constructor() {
         this.instantiateSliders();
+        this.loadPreferences();
     }
 
     instantiateSliders() {
-        this.morningslider = $('#slider_morning').slider();
-        this.nightslider = $('#slider_night').slider();
-        this.consecutiveslider = $('#slider_consecutive').slider();
-        this.rmpslider = $('#slider_rmp').slider();
+        var self = this;
+
+        self.morningslider = $('#slider_morning').slider()
+                                .on('slideStop', function () {
+                                    self.savePreferences();
+                                });
+
+        self.nightslider = $('#slider_night').slider()
+                                .on('slideStop', function () {
+                                    self.savePreferences();
+                                });
+        self.consecutiveslider = $('#slider_consecutive').slider()
+                                .on('slideStop', function () {
+                                    self.savePreferences();
+                                });
+        self.rmpslider = $('#slider_rmp').slider()
+                                .on('slideStop', function () {
+                                    self.savePreferences();
+                                });
     }
 
     getMorningValue() {
@@ -24,5 +40,41 @@ class Preferences {
 
     getRMPValue() {
         return this.rmpslider.slider('getValue');
+    }
+    
+    setMorningValue(value) {
+        if (value != null) this.morningslider.slider('setValue', parseInt(value));
+    }
+
+    setNightValue(value) {
+        if (value != null) this.nightslider.slider('setValue', parseInt(value));
+    }
+
+    setConsecutiveValue(value) {
+        if (value != null) this.consecutiveslider.slider('setValue', parseInt(value));
+    }
+
+    setRMPValue(value) {
+        if (value != null) this.rmpslider.slider('setValue', parseInt(value));
+    }    
+
+    /*
+        Saves the current slider values to localStorage
+    */
+    savePreferences() {
+        localStorage.setItem('morningslider', this.getMorningValue());
+        localStorage.setItem('nightslider', this.getNightValue());
+        localStorage.setItem('consecutiveslider', this.getConsecutiveValue());
+        localStorage.setItem('rmpslider', this.getRMPValue());
+    }
+
+    /*
+        If there are saved preferences in localStorage, this loads them
+    */
+    loadPreferences() {
+        this.setMorningValue(localStorage.getItem('morningslider'));
+        this.setNightValue(localStorage.getItem('nightslider'));
+        this.setConsecutiveValue(localStorage.getItem('consecutiveslider'));
+        this.setRMPValue(localStorage.getItem('rmpslider'));
     }
 }
