10f0ca690b106ddfa60b2091b2b70eea76dae6fa
Step7750 <Step7750@gmail.com>
2016-10-01 20:18:20 -0600

    Removes Redundant isConflicting Function

--- js/Generator.js
+++ js/Generator.js
@@ -736,24 +736,6 @@ class Generator {
         }
     }
 
-    static isConflicting(time1, time2) {
-        /*
-            time1 and time2 are arrays with the first index being the total minutes 
-            since 12:00AM that day of the starttime and the second being the endtime
-            ex. [570, 645] and [590, 740]
-            We check whether the end time of time2 is greater than the start time of time1
-            and whether the end time of time1 is greater than the start time of time2
-            if so, there is a conflict
-        */
-
-        if (time1[1] > time2[0] && time2[1] > time1[0]) {
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-
     /*
         Converts the format of the blockedTimes to the total minutes format used by the generator
     */
